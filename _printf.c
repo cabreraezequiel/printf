9ee74e89b2c539e613d4a35655412e3a4dc2460a
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-03-11 10:28:39 -0800

    Added _printf.c

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0, int = 0;
+	va_list ap;
+	str
+		va_struct type[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	va_start(ap, string?)
+		while
+		{
+			str = (va_arg(ap, char *));
+			if string[n] == %;
+			n++;
+			while
+				if string[n] == type[i];
+			symbol[i].fun(ap);
+			count ++;
+			else
+				print_char(string[n]);
+		}
+}
