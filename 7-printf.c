2a497e5d004f40a0bf50f998a21edd41d28a1425
oxyvans <benja.1999.g@hotmail.com>
2022-03-15 05:43:28 -0700

    7

--- 7-printf.c
+++ 7-printf.c
@@ -11,35 +11,40 @@ int print_S(va_list S)
 	char hexa[] = "0123456789ABCDEF";
 	char *aux = va_arg(S, char *);
 	int i = 0, j = 0;
-	unsigned long int n = 0;
+	int n = 0;
 	int a[10];
 
-	if (aux == 0)
+	if (!aux)
 	{
-		return (0);
+		_putchar('(');
+		_putchar('n');
+		_putchar('i');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (5);
 	}
 	for (i = 0; aux[i]; i++)
 	{
 		n = aux[i];
-		if ((n > 0 && n < 32) || (n >= 127))
+		if (!(n >= 32 && n < 127))
 		{
 			_putchar('\\');
 			_putchar('x');
-			_putchar(((n / 16) % 10) + '0');
 			for (j = 0; n > 0; j++)
 			{
 				a[j] = n % 16;
 				n = n / 16;
 			}
+			a[j] = n % 16;
+			j++;
 			for (j -= 1; j >= 0; j--)
 			{
 				_putchar(hexa[a[j]]);
 			}
 		}
 		else
-		{
 			_putchar(aux[i]);
-		}
 	}
 	return (0);
 }
